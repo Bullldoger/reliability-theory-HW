3c3cc813f0afd49a00b5e15035eb7be4eb43e511
hegnatin <kaire1996@gmail.com>
2017-11-11 16:42:10 +0300

    added trash-file

---
+++ cons.pl
@@ -0,0 +1,15 @@
+conjugate([Re, Im], [Re, Im_n]) :- Im_n is Im * (-1).
+
+get_column(_ColumnNumber, [], []):-!.
+get_column(ColumnNumber, [Row|Tail], [ElementConj|TailColumn]):-
+  nth0(ColumnNumber, Row, Element), conjugate(Element, ElementConj),
+  get_column(ColumnNumber, Tail, TailColumn).
+
+transposition(Matrix, TransposeMatrix):-
+  transposition(0, Matrix, TransposeMatrix).
+ 
+transposition(Iterator, Matrix, [Column|TransposeTail]):-
+  TailIterator is Iterator + 1,
+  get_column(Iterator, Matrix, Column), !,
+  transposition(TailIterator, Matrix, TransposeTail).
+transposition(_Iterator, _Matrix, []).
