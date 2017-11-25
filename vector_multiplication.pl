06bd4bea1c4d61e9844440e9af2de4bc7988074a
Revan <kaire1996@gmail.com>
2017-11-25 23:18:17 +0300

    added new prolog-file

---
+++ vector_multiplication.pl
@@ -0,0 +1,16 @@
+
+%Реализация скалярного произведения
+%Если входной список пуст, то отсечение
+scalar_multiplication(_, [], []) :- !.
+
+%к-ый элемент умножается на K, далее рекурсивно вниз.
+scalar_multiplication(K, [H|T], [H1|T1]) :- 
+									H1 is H * K, 
+									scalar_multiplication(K, T, T1).
+
+%Если входной список пуст, то отсечение
+vector_multiplication([], _, []) :- !.
+%Элемент H умножается на вектор строку Y и дописывается в новый список, далее рекурсивно вниз
+vector_multiplication([H|T], Y, [ANS_H|ANS_T]) :- 
+											scalar_multiplication(H, Y, ANS_H), 
+											vector_multiplication(T, Y, ANS_T).
