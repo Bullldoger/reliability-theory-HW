0072cd5e3e8c4acae3f1d3d2390e44737202cbf1
Roman <kaire1996@gmail.com>
2017-12-20 23:30:56 +0300

    Changed labs

--- vector_multiplication.pl
+++ vector_multiplication.pl
@@ -1,11 +1,11 @@
-
+:- use_module(library(clpfd)).
 %Реализация скалярного произведения
 %Если входной список пуст, то отсечение
 scalar_multiplication(_, [], []) :- !.
 
 %к-ый элемент умножается на K, далее рекурсивно вниз.
-scalar_multiplication(K, [H|T], [H1|T1]) :- 
-									H1 is H * K, 
+scalar_multiplication(K, [H|T], [H1|T1]) :-
+									H1 #= H * K, 
 									scalar_multiplication(K, T, T1).
 
 %Если входной список пуст, то отсечение
