4acafbef5d8516a924e73c8d85d5be6ed9d09a74
Roman <kaire1996@gmail.com>
2018-01-13 16:24:35 +0300

    added checking matrix elems:

--- cons.pl
+++ cons.pl
@@ -6,7 +6,17 @@ get_column(ColumnNumber, [Row|Tail], [ElementConj|TailColumn]):-
   nth0(ColumnNumber, Row, Element), conjugate(Element, ElementConj),
   get_column(ColumnNumber, Tail, TailColumn).
 
+checkMatrixRowElements([]).
+checkMatrixRowElements([H|T]) :- length(H, N),
+								 N = 2,
+								 checkMatrixRowElements(T).
+checkMatrix([]).
+checkMatrix([H|T]) :- checkMatrix(T),
+					  checkMatrixRowElements(H).
+
+
 transposition(Matrix, TransposeMatrix):-
+  checkMatrix(Matrix),
   transposition(0, Matrix, TransposeMatrix).
  
 transposition(Iterator, Matrix, [Column|TransposeTail]):-
